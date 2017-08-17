f0a37684bfb3cab1027e04e9fa9cecf0ea1082fe
ducaale <sharaf.13@hotmail.com>
2017-08-17 20:20:44 +0300

    new bullet sprite

--- src/game.cpp
+++ src/game.cpp
@@ -50,7 +50,7 @@ Entity& Game::createRightArm(Entity& parent) {
     entity.addComponent<CTransform>(sf::Vector2f(0.f,-100.f));
     entity.addComponent<CParent>(&parent);
     entity.addComponent<CSprite>(this, sf::Sprite(resource["orb"], {384,128,128,32}));
-    entity.addComponent<CGun>(this, sf::Sprite(resource["orb"], {0,256,64,32}), 2.f, 200.f);
+    entity.addComponent<CGun>(this, sf::Sprite(resource["orb"], {0,256,32,16}), 2.f, 200.f);
     entity.addComponent<COrbArmBehaviour>();
 
     entity.addGroup(Groups::drawable);
@@ -63,7 +63,7 @@ Entity& Game::createLeftArm(Entity& parent) {
     entity.addComponent<CTransform>(sf::Vector2f(0.f,100.f));
     entity.addComponent<CParent>(&parent);
     entity.addComponent<CSprite>(this, sf::Sprite(resource["orb"], {256,128,128,32}));
-    entity.addComponent<CGun>(this, sf::Sprite(resource["orb"], {0,256,64,32}), 2.f, 200.f);
+    entity.addComponent<CGun>(this, sf::Sprite(resource["orb"], {0,256,32,16}), 2.f, 200.f);
     entity.addComponent<COrbArmBehaviour>();
 
     entity.addGroup(Groups::drawable);
